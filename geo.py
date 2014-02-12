fd69c41dbc08e6fe57fc05efc0386adec19696fa
Matt Gross <mattkgross@gmail.com>
2014-02-11 17:08:50 -0700

    Andrew's Sample Twitter Feed Script

---
+++ geo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import twitter
+import json
+
+api = twitter.Api(consumer_key='kt3lmNFHN6p7MVfc0PMg',consumer_secret='3JvBprZAawaXJoGpqequIeBMJx5ibZW7Wqc6FquoILU', access_token_key='58371709-WbrfIEF0EVONTkaxiboYcTGOy5arWKtAd4MDbBQkK', access_token_secret='nAQdXoWYryK3LvlM3Nztj6doj1ciU9MOWLdLqdN0MWPgJ',cache=None)
+
+
+statuses = api.GetStreamSample()
+for s in statuses:
+	if 'text' in s and 'geo' in s:
+		if s['geo'] !=None:
+			print s['text'], s['geo']
+			continue
+			
+#print json.dumps(s)
