711e1c23aa036ca94da992addd5d4f4f56710602
Matthew Gross <mattkgross@gmail.com>
2014-02-18 17:44:36 -0700

    Help file for Twitter API

--- Firehose.py
+++ Firehose.py
@@ -5,10 +5,10 @@ import json
 import pprint
 
 # Andrew's API Credentials
-api = twitter.Api(consumer_key='kt3lmNFHN6p7MVfc0PMg',consumer_secret='3JvBprZAawaXJoGpqequIeBMJx5ibZW7Wqc6FquoILU', access_token_key='58371709-WbrfIEF0EVONTkaxiboYcTGOy5arWKtAd4MDbBQkK', access_token_secret='nAQdXoWYryK3LvlM3Nztj6doj1ciU9MOWLdLqdN0MWPgJ',cache=None)
+#api = twitter.Api(consumer_key='kt3lmNFHN6p7MVfc0PMg',consumer_secret='3JvBprZAawaXJoGpqequIeBMJx5ibZW7Wqc6FquoILU', access_token_key='58371709-WbrfIEF0EVONTkaxiboYcTGOy5arWKtAd4MDbBQkK', access_token_secret='nAQdXoWYryK3LvlM3Nztj6doj1ciU9MOWLdLqdN0MWPgJ',cache=None)
 
 # Matt's API Credentials for alternate use
-# api = twitter.Api(consumer_key='DdU16WP4VM7P9PHOUJ57g', consumer_secret='s3MRayg34QV82RXOsG3VZquXY0JY7k0osL5SfKZ2o', access_token_key='419788290-Lq3CrrkXbRhv6Sdn06KDCQRueErUpXCF8dFZY2wo', access_token_secret='wcmsAk7CUrJHifaTcZiAVsN4tCC4rmV8h59rw2ZUeO3S6', cache=None)
+api = twitter.Api(consumer_key='DdU16WP4VM7P9PHOUJ57g', consumer_secret='s3MRayg34QV82RXOsG3VZquXY0JY7k0osL5SfKZ2o', access_token_key='419788290-Lq3CrrkXbRhv6Sdn06KDCQRueErUpXCF8dFZY2wo', access_token_secret='wcmsAk7CUrJHifaTcZiAVsN4tCC4rmV8h59rw2ZUeO3S6', cache=None)
 
 
 statuses = api.GetStreamSample()
@@ -16,10 +16,11 @@ statuses = api.GetStreamSample()
 # Prints a single tweet's JSON inline - useful for analyzing structure of a tweet
 #pp = pprint.PrettyPrinter(indent=4)
 #for obj in statuses:
-#	print pp.pprint(obj)
-#	break
+#	if 'text' in obj and obj['text'] != None and 'lang' in obj and obj['lang'] == 'en' and obj['entities']['hashtags'] != None:
+#		print pp.pprint(obj)
+#		break
 
-# Iterate through all geolocated tweets
+ Iterate through all geolocated tweets
 for obj in statuses:
 	if 'text' in obj and 'coordinates' in obj and 'lang' in obj:
 		if obj['text'] != None and obj['coordinates'] != None and obj['lang'] == "en":
