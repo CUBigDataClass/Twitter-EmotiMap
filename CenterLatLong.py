5a7129302cc30448c6583dd5d6d557e8cafb14df
Dillon Fancher <daffancher@gmail.com>
2014-02-25 16:58:05 -0700

    Started Python to analyze Lat,Long Mathematically

---
+++ CenterLatLong.py
@@ -0,0 +1,18 @@
+import twitter
+import json
+import pprint
+
+tweet_json = []
+
+with open('SampleMongoDb.json') as f:
+    for line in f:
+        tweet_json.append(json.loads(line))
+
+geo =[]
+count = 0
+for obj in tweet_json:
+    count = count+1
+    geo.append(obj['coordinates']['coordinates'])
+
+
+
