cf45a97a77c8f835af9b6cc2fc09c5d9a669f05e
Dillon Fancher <daffancher@gmail.com>
2014-02-26 02:13:43 -0700

    Parse tweets into respective array of words, trouble with Unicode being recognized still

--- CenterLatLong.py
+++ CenterLatLong.py
@@ -1,6 +1,10 @@
 import twitter
 import json
 import pprint
+import enchant
+
+
+#still a work in progress, just have the geo-location being put into an array
 
 tweet_json = []
 
@@ -8,11 +12,11 @@ with open('SampleMongoDb.json') as f:
     for line in f:
         tweet_json.append(json.loads(line))
 
-geo =[]
+geo = []
 count = 0
 for obj in tweet_json:
     count = count+1
     geo.append(obj['coordinates']['coordinates'])
 
-
+print(geo)
 
