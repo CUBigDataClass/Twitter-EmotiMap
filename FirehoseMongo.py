3cbf1d832724b9caf097caf43d8d0b6118de54b0
Matt Gross <mattkgross@gmail.com>
2014-02-25 17:21:25 -0700

    Firehose FIlter Added

--- FirehoseMongo.py
+++ FirehoseMongo.py
@@ -6,16 +6,16 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.test
 
-api = twitter.Api(consumer_key='kt3lmNFHN6p7MVfc0PMg',consumer_secret='3JvBprZAawaXJoGpqequIeBMJx5ibZW7Wqc6FquoILU', access_token_key='58371709-WbrfIEF0EVONTkaxiboYcTGOy5arWKtAd4MDbBQkK', access_token_secret='nAQdXoWYryK3LvlM3Nztj6doj1ciU9MOWLdLqdN0MWPgJ',cache=None)
+api = twitter.Api(consumer_key='DdU16WP4VM7P9PHOUJ57g', consumer_secret='s3MRayg34QV82RXOsG3VZquXY0JY7k0osL5SfKZ2o', access_token_key='419788290-Lq3CrrkXbRhv6Sdn06KDCQRueErUpXCF8dFZY2wo', access_token_secret='wcmsAk7CUrJHifaTcZiAVsN4tCC4rmV8h59rw2ZUeO3S6', cache=None)
 
 
 statuses = api.GetStreamSample()
-for s in statuses:
-	if 'text' in s and 'geo' in s:
-		if s['geo'] !=None:
-			print s['text'], s['geo']
-			post = s
-			db.TweetsGeo.insert(post)
+
+#Iterate through all geolocated tweets
+for obj in statuses:
+	if 'text' in obj and 'coordinates' in obj and 'lang' in obj and 'country_code' in obj:
+		if obj['text'] != None and obj['coordinates'] != None and obj['place']['country_code'] == "US" and obj['lang'] == "en":
+			db.TweetsGeo.insert(obj)
 			continue
 			
 #print json.dumps(s)
