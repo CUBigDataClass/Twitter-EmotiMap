d78f5f30191589ae9a27347250b6a3108d23e87c
Dillon Fancher <daffancher@gmail.com>
2014-02-27 14:43:35 -0700

    uh

--- USAmap.R
+++
@@ -1,9 +0,0 @@
-library(maps)
-LatLong <- read.csv("~/Documents/School/Computing/Twitter-EmotiMap/LatLong.csv", header=F)
-map('world')
-points(x = LatLong$V1, y = LatLong$V2, col = rgb(runif(5),runif(5),runif(5)) , pch = 20, cex = 1.0)
- 
-
-
-
-
