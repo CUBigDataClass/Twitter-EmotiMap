24df0c1cd925f48c443d45cc0f6a944c4bf1fef4
Dillon Fancher <daffancher@gmail.com>
2014-02-28 11:50:44 -0700

    New updated map with 1400 points

--- WorldMap.R
+++ WorldMap.R
@@ -1,6 +1,8 @@
 library(maps)
 LatLong <- read.csv("~/Documents/School/Computing/Twitter-EmotiMap/LatLong.csv", header=F)
-map('world')
+PercentLit <- read.csv("~/Documents/School/Computing/Twitter-EmotiMap/PercentLit.csv", header=F)
+map('usa')
+map('state')
 points(x = LatLong$V1, y = LatLong$V2, col = rgb(runif(5),runif(5),runif(5)) , pch = 20, cex = 1.0)
  
 
