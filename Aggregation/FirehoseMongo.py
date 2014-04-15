def066bee3f584be46bb7c58ea8911a9e9b23a04
Dillon Fancher <daffancher@gmail.com>
2014-04-15 12:48:38 -0600

    made changes but deleted them so this is still the same

--- Aggregation/FirehoseMongo.py
+++ Aggregation/FirehoseMongo.py
@@ -6,6 +6,7 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.test
 
+
 #this is Dillon Fancher's API key
 api = twitter.Api(consumer_key='G1ztnAQmyriiOqcyt9pGQ', consumer_secret='TzNhT5oCWMMU5eIk6E61bgSuXQ7qDHuYv5EbYwDKQ', access_token_key='572163554-le5J2mX0lb5IMOdAVPyAkxQ8M24UIMIyQMafqyur', access_token_secret='EB9xZBqfRVuga9tUsrqiP5qf5PHCsvsGrm3UkHz3nyZPc', cache=None)
 
@@ -19,11 +20,10 @@ while(True):
 			if 	place != None and 'country_code' in place:
 				if obj['text'] != None and obj['coordinates'] != None and obj['place']['country_code'] == "US" and obj['lang'] == "en":
 					# Grab the user who created the tweet
-					print "Username: %s" % (obj['user']['name'])
+					#print "Username: %s" % (obj['user']['name'])
 					# Record the tweet
-					print "Tweet: %s\nLocation: (%f, %f)\n" % (obj['text'], obj['coordinates']['coordinates'][0], obj['coordinates']['coordinates'][1])
+					#print "Tweet: %s\nLocation: (%f, %f)\n" % (obj['text'], obj['coordinates']['coordinates'][0], obj['coordinates']['coordinates'][1])
 					db.TweetsGeo.update({'id' : obj['id']}, obj, True)
-	
 					continue
 			
 #print json.dumps(s)
